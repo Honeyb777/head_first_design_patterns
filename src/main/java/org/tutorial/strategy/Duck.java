fa28b2d7a540e9b5799eb5443e93a5b8bc74c01b
sangyun lee <sangyun202@gmail.com>
2023-10-23 01:13:44 +0900

    [추가] Duck 클래스에서 개별 객체의 행동을 직접 처리하지 않고 참조 객체를 통해 사용하고, 개별 객체의 생성자에서 행동을 상속받음

---
+++ src/main/java/org/tutorial/strategy/Duck.java
@@ -0,0 +1,31 @@
+package org.tutorial.strategy;
+
+public abstract class Duck  {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public Duck() {
+    }
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+
+    abstract void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("모든 오리는 수영함, 목각 오리도 물에 떠있음");
+    }
+}
--- src/main/java/org/tutorial/strategy/README.md
+++ src/main/java/org/tutorial/strategy/README.md
@@ -6,3 +6,11 @@
 2. 오리의 fly 메소드와 quack 메소드를 행동이라고 표현하고 인터페이스화 한다
    - 오리의 나는 행동은 FlyBehavior 인터페이스
    - 오리의 소리내는 행동은 QuackBehavior 인터페이스
+# Step2 오리 행동 통합하기
+1. 오리의 나는 행동과 소리내는 행동을 Duck 클래스에서 정의한 메소드를 써서 구현하지 않고 다른 클래스에 위임
+   - flyBehavior 와 quackBehavior 인터페이스 형식의 인스턴스 변수를 추가\
+     (특정 구상 클래스 형식으로 선언하지 않음)
+2. 각 오리 객체에서는 실행시에 이 변수에 특정 행동 형식(FlyWithWings, Squeak 등)의 레퍼런스를 다형적으로 설정하여 사용
+3. 나는 행동과 소리내는 행동은 Duck 클래스에서 직접 처리하지 않고, performFly 메소드와 performQuack() 메소드에 의해 참조되는 객체를 통해 사용
+4. MallardDuck 인스턴스 변수의 경우 생성자에서 quackBehavior 인스턴스 변수와 flyBehavior 인스턴스 변수를 상속받음
+   - 이때 타입은 Quack 형식과 FlyWithWings 형식을 사용함 
